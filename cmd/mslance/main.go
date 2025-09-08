78d6e78b7ecd1b613357b62c87f3a0450bc1aa13
Guilherme Peruci <guilherme.f.peruci@gmail.com>
2025-09-08 19:27:33 -0300

    msleilao
    bum boom

---
+++ cmd/mslance/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"auction-system/internal/mslance"
+	"auction-system/pkg/rabbitmq"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	conn, ch := rabbitmq.Connect()
+	defer conn.Close()
+	defer ch.Close()
+
+	msl := mslance.NewMSLance(ch)
+
+	msl.DeclareQueues()
+	msl.ListenLeilaoIniciado()
+	msl.ListenLanceRealizado()
+	msl.ListenLeilaoFinalizado()
+
+	forever := make(chan os.Signal, 1)
+	signal.Notify(forever, os.Interrupt)
+	<-forever
+}
